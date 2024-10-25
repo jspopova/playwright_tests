68535f42b1fcbb787a87121958e38591528e8126
Yuliya Popova <mandrichenko2605@yandex.ru>
2024-10-25 17:50:47 +0300

    pages added, demo test fixed

--- tests/demo.spec.js
+++ tests/demo.spec.js
@@ -3,37 +3,42 @@ import { faker } from '@faker-js/faker'
 import { before } from 'node:test';
 
 const url='https://realworld.qa.guru/'
-
+let newUser;
  
 test.describe.only('Профиль пользователя', ()=> { //это тест сьют,группа тестов, используется для группировки
 
   test.beforeEach ('Регистрация пользователя перед каждым тестом', async ({ page }) => {
-    let womenProfileArray=[]
+  //  let womenProfileArray=[]
+    
     await page.goto(url);
-    let userName = faker.person.firstName('female');
-    let userEmail = faker.internet.email();
-    let userPassword = faker.internet.password();
+    newUser ={
+      userBio: faker.music.genre(),
+      userEmail: faker.internet.email(),
+      userName: faker.person.firstName('female'),
+      userPassword: faker.internet.password()
+    }
+    
     await page.getByRole('link', { name: 'Sign up' }).click();
-    await page.getByPlaceholder('Your Name').fill(userName);
+    await page.getByPlaceholder('Your Name').fill(newUser.userName)
     await page.getByPlaceholder('Your Name').press('Tab');
     await page.getByPlaceholder('Email').click();
   
-    await page.getByPlaceholder('Email').fill(userEmail);
+    await page.getByPlaceholder('Email').fill(newUser.userEmail);
     await page.getByPlaceholder('Password').click();
-    await page.getByPlaceholder('Password').fill(userPassword);
+    await page.getByPlaceholder('Password').fill(newUser.userPassword);
     await page.getByRole('button', { name: 'Sign up' }).click();
-    await page.getByText(userName).click();
+    await page.getByText(newUser.userName).click();
    // await expect(page.getByRole('navigation')).toContainText(userName); expect  в beforeEach не нужен
   });
-//!!! beforeEach будет в хукахб что эо значит
+//!!! beforeEach будет в хуках что это значит
 test('Изменение bio пользователя', async ({ page }) => { //test это функция
- let shortUserBio=faker.music.genre();
+ //let shortUserBio=faker.music.genre();
   await page.locator('.dropdown-toggle').click;
   await page.getByRole('link', { name: 'Settings' }).click();
   await page.getByPlaceholder('Short bio about you').click();
-  await page.getByPlaceholder('Short bio about you').fill(shortUserBio);
+  await page.getByPlaceholder('Short bio about you').fill(newUser.userBio);
   await page.getByRole('button', 'Update Settings')
-  await expect(page.getByPlaceholder('Short bio about you')).toContainText(shortUserBio);
+  await expect(page.getByPlaceholder('Short bio about you')).toContainText(newUser.userBio);
 
   /*await page.getByRole('link', { name: 'Sign up' }).click();
   await page.getByPlaceholder('Your Name').fill(userName);
